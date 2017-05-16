147f1ee13620ed99cc15b56fd4efadff7459d349
赖文强 <1260091093@qq.com>
2017-05-16 18:21:19 +0800

    Create Response.java

---
+++ src/com/ex01/pyrmont/Response.java
@@ -0,0 +1,64 @@
+package com.ex01.pyrmont;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by laiwenqiang on 2017/5/16.
+ */
+public class Response {
+    private static final int BUFFER_SIZE = 1024;
+    Request request;
+    OutputStream output;
+
+    public Response(OutputStream output) {
+        this.output = output;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public void sendStaticResource() throws IOException {
+        byte[] bytes = new byte[BUFFER_SIZE];
+        FileInputStream fis = null;
+        try {
+            File file = new File(HttpServer.WEB_ROOT, request.getUri());
+            if (file.exists()) {
+                fis = new FileInputStream(file);
+                int ch = fis.read(bytes, 0, BUFFER_SIZE);
+
+                //源代码中没有下面这段话，导致请求会出现错误。现在添加。
+                String msg = "HTTP/1.1 404 File Not Found\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n";
+                output.write(msg.getBytes());
+
+                while (ch!=-1) {
+                    output.write(bytes, 0, ch);
+                    ch = fis.read(bytes, 0, BUFFER_SIZE);
+                }
+            }
+            else {
+                // file not found
+                String errorMessage = "HTTP/1.1 404 File Not Found\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: 23\r\n" +
+                        "\r\n" +
+                        "<h1>File Not Found</h1>";
+                output.write(errorMessage.getBytes());
+            }
+        }
+        catch (Exception e) {
+            // thrown if cannot instantiate a File object
+            System.out.println(e.toString() );
+        }
+        finally {
+            if (fis!=null)
+                fis.close();
+        }
+    }
+
+}
